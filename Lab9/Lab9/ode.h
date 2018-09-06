05c478f943f8aa632cb78d4b33af16cef933b885
Bianca <33526317+biafrag@users.noreply.github.com>
2018-09-06 10:41:50 -0300

    Ajustando

--- Lab9/Lab9/ode.h
+++
@@ -1,5 +0,0 @@
-double Euler (double t0, double t1, double h, double x0,
-double (*f) (double t, double x));
-
-double EulerAd (double t0, double t1, double h0, double x0,
-double (*f) (double t, double x), double tol);
